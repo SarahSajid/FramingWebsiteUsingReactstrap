3e879455909ec87686037b6d6023e55303f9c576
SarahSajid <13besessajid@seecs.edu.pk>
2020-04-30 16:43:32 +0500

    Add create react app boilerplate code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from './nav.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
